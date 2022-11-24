e08654f57b428e7458de684bab3c2f72e2df4067
SkyRaazz <115127423+SkyRaazz@users.noreply.github.com>
2022-11-24 10:49:45 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String salam = "Assalamualaikum";
+String nama = "M. Andra Alfarizi";
+int lahir = 2003;
+
+System.out.println(salam);
+System.out.println("Nama Saya "+nama);
+System.out.println("Saya Lahir di Tahun : "+lahir);
+    }
+}
